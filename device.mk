52d01f443a4f699eb766ee575ae96fe5100e6838
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:39:26 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -112,3 +112,4 @@ PRODUCT_PACKAGES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nubia/nx563j/nx563j-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
